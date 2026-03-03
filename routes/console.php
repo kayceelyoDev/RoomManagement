fcec036778a7eec94c2c69cb77ab7ba6d34a541b
Kent clarence Evangelista <kentclarence5368@gmail.com>
2026-03-03 22:07:52 +0800

    fix ram issue

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('reservations:cancel-pending')->everyMinute();
+// Run every hour instead of every minute to reduce RAM usage
+// Since reservations are cancelled after 2 hours, hourly checks are sufficient
+Schedule::command('reservations:cancel-pending')->everyTenMinutes();
